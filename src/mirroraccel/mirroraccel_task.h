3c560fdf497ec0fb766aa32ce4348c88b1b6f0e6
sosopop <cabbage1102@gmail.com>
2019-07-08 16:22:51 +0800

    二期修改

--- src/mirroraccel/mirroraccel_task.h
+++ src/mirroraccel/mirroraccel_task.h
@@ -1,66 +1,79 @@
 #ifndef MIRRORACCEL_TASK_H_
 #define MIRRORACCEL_TASK_H_
 
-#include<chrono>
+#include <chrono>
 #include <mutex>
 #include "mongoose.h"
 
 namespace mirroraccel
 {
-    enum {
-        TASK_INIT_SIZE = 32 * 1024,
-        TASK_DATA_SIZE = 1024 * 1024
-    };
-    class ConnOutgoing;
-    struct Task
+enum
+{
+    TASK_INIT_SIZE = 32 * 1024,
+    TASK_MAX_BUFFER_SIZE = 1024 * 1024,
+    TASK_DATA_SIZE = 1024 * 1024
+};
+class ConnOutgoing;
+struct Task
+{
+    Task(std::int64_t rangeStart, std::int64_t rangeSize)
+        : rangeStart(rangeStart), rangeSize(rangeSize)
     {
-        Task(std::int64_t rangeStart, std::int64_t rangeSize)
-            : rangeStart(rangeStart), rangeSize(rangeSize) {
-            mbuf_init(&buffer, TASK_INIT_SIZE);
-        }
-        bool operator < (const Task& task) const
+        mbuf_init(&buffer, TASK_INIT_SIZE);
+    }
+    bool operator<(const Task &task) const
+    {
+        return this->rangeStart < task.rangeStart;
+    }
+    void read(mbuf &buffer)
+    {
+        std::lock_guard<std::mutex> lock(bufferMux);
+        buffer = this->buffer;
+        mbuf_init(&this->buffer, TASK_INIT_SIZE);
+    }
+    size_t writeHeader(char *data, size_t len)
+    {
+        std::lock_guard<std::mutex> lock(bufferMux);
+        mbuf_append(&buffer, data, len);
+        return buffer.len;
+    }
+    size_t writeData(char *data, size_t len)
+    {
+        std::lock_guard<std::mutex> lock(bufferMux);
+        if (rangeCurSize + len > rangeSize)
+            len = rangeSize - rangeCurSize;
+
+        std::int64_t pos = rangeStart + rangeCurSize;
+        for (std::int64_t i = 0; i < len; i++)
         {
-            return this->rangeStart < task.rangeStart;
+            std::int64_t c = pos + i + 1;
+            c = c ^ (c << 1) ^ (c << 2) ^ (c << 4) ^ (c << 6) ^ (c >> 1) ^ (c >> 2) ^ (c >> 4) ^ (c >> 6) ^ (c >> 12);
+            data[i] = data[i] ^ (unsigned char)(c) ^ "hello"[(pos + i) % 5];
         }
-        void read(mbuf& buffer) {
-            std::lock_guard<std::mutex> lock(bufferMux);
-            buffer = this->buffer;
-            mbuf_init(&this->buffer, TASK_INIT_SIZE);
-        }
-        size_t writeHeader(char* data, size_t len) {
-            std::lock_guard<std::mutex> lock(bufferMux);
-            mbuf_append(&buffer, data, len);
-            return buffer.len;
-        }
-        size_t writeData(char* data, size_t len) {
-            std::lock_guard<std::mutex> lock(bufferMux);
 
-            std::int64_t pos = rangeStart + rangeCurSize;
-            for (std::int64_t i = 0; i < len; i++)
-            {
-                std::int64_t c = pos + i + 1;
-                c = c ^ (c << 1) ^ (c << 2) ^ (c << 4) ^ (c << 6) ^ (c >> 1) ^ (c >> 2) ^ (c >> 4) ^ (c >> 6) ^ (c >> 12);
-                data[i] = data[i] ^ (unsigned char)(c) ^ "hello"[(pos + i) % 5];
-            }
-
-            mbuf_append(&buffer, data, len);
-            rangeCurSize += len;
-            return buffer.len;
-        }
-        size_t size() {
-            std::lock_guard<std::mutex> lock(bufferMux);
-            size_t len = buffer.len;
-            return len;
-        }
-        std::int64_t rangeStart = 0;
-        std::int64_t rangeSize = 0;
-        std::int64_t rangeCurSize = 0;
-        //开始时间戳
-        std::int64_t startTime = std::chrono::steady_clock::now().time_since_epoch().count();
-        mbuf buffer;
-        std::mutex bufferMux;
-    };
-} 
+        mbuf_append(&buffer, data, len);
+        rangeCurSize += len;
+        return buffer.len;
+    }
+    size_t size()
+    {
+        std::lock_guard<std::mutex> lock(bufferMux);
+        size_t len = buffer.len;
+        return len;
+    }
+    bool finished()
+    {
+        return rangeCurSize == rangeSize;
+    }
+    std::int64_t rangeStart = 0;
+    std::int64_t rangeSize = 0;
+    std::int64_t rangeCurSize = 0;
+    //开始时间戳
+    std::int64_t startTime = std::chrono::steady_clock::now().time_since_epoch().count();
+    mbuf buffer;
+    std::mutex bufferMux;
+};
+} // namespace mirroraccel
 // namespace mirroraccel
 
 #endif
